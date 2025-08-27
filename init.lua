3fdc77ed9883b706ed941becd8b1cead782acd3d
Chong Yao <56928030+mahopon@users.noreply.github.com>
2025-08-27 21:20:07 +0800

    Add nobomb

--- init.lua
+++ init.lua
@@ -20,6 +20,9 @@ vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.opt.wrap = true
 vim.o.number = true
+vim.o.encoding = "utf-8"
+vim.o.fileencoding = "utf-8"
+vim.bo.bomb = false
 
 -- Keybindings
 vim.api.nvim_set_keymap('n', 'j', 'k', { noremap = true, silent = true })
