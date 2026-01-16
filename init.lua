bf1e663fd494202839df35b5240cc1ec4cd650ff
Chong Yao <tan.cy01@outlook.com>
2026-01-16 10:24:43 +0800

    Update diagnostic to appear in window

--- init.lua
+++ init.lua
@@ -134,9 +134,13 @@ vim.api.nvim_create_autocmd("VimEnter", {
 })
 
 vim.diagnostic.config({
-  virtual_text = true,
+  virtual_text = false,
   signs = true,
   underline = true,
   update_in_insert = false,
   severity_sort = true,
 })
+
+-- Show line diagnostics automatically in hover window
+vim.o.updatetime = 250
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
