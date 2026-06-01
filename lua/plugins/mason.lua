a06250ddcf9deb4698e875d3e005187600b93e1e
mahopon <tan.cy01@outlook.com>
2026-06-01 22:21:23 +0800

    Add ansible ls and linting

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -13,6 +13,38 @@ return {
                         },
                     },
                 }
+
+                vim.lsp.config["ansiblels"] = {
+                    filetypes = { "yaml.ansible" },
+                    settings = {
+                        ansible = {
+                            path = "ansible",
+                            python = {
+                                interpreterPath = "python3",
+                            },
+                            validation = {        -- moved inside ansible {}
+                                enabled = true,
+                                lint = {
+                                    enabled = true,
+                                    path = "ansible-lint",
+                                },
+                            },
+                        },
+                    },
+                }
+
+                vim.filetype.add({
+                    pattern = {
+                        -- playbooks
+                        [".*/playbooks/.*%.yml"]  = "yaml.ansible",
+                        [".*/playbooks/.*%.yaml"] = "yaml.ansible",
+                        -- roles
+                        [".*/roles/.*/tasks/.*%.ya?ml"]    = "yaml.ansible",
+                        [".*/roles/.*/handlers/.*%.ya?ml"] = "yaml.ansible",
+                        [".*/roles/.*/defaults/.*%.ya?ml"] = "yaml.ansible",
+                        [".*/roles/.*/vars/.*%.ya?ml"]     = "yaml.ansible",
+                    },
+                })
             end,
         },
     },
